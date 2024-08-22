2d84aa8fa564515e1767bfa3bbacdbe550a478b6
jiwangyihao <jwyh@jwyihao.top>
2024-08-22 22:08:19 +0800

    feat(50%): animation support

--- packages/vue-core/src/animation.ts
+++ packages/vue-core/src/animation.ts
@@ -1,13 +1,27 @@
 import { Ref, VNodeRef } from "vue"
 
 export interface AnimationContext<T> {
-  props: T
+  props: Record<string, Ref>
 }
 
 export interface Animation<T> {
   init(context: AnimationContext<T>): void
-  update(context: AnimationContext<T>) : void
-  finish(context: AnimationContext<T>) : void
+
+  update(context: AnimationContext<T>): void
+
+  finish(context: AnimationContext<T>): void
+
   duration: number
-  by?: number
+  by?: (progress: number) => number
+  progress?: number
+  begin?: number
+}
+
+export function defineAnimation<T>(animation: Animation<T>) {
+  return animation
+}
+
+export interface BaseAnimation {
+  duration: number
+  by?: (progress: number) => number
 }
--- packages/vue-test/src/App.vue
+++ packages/vue-test/src/App.vue
@@ -1,15 +1,17 @@
 <script setup lang="ts">
-import { usePlayer, useWidget } from '@vuemotion/vue-core'
-import { Car, Arc } from '@vuemotion/vue-lib'
-import { nextTick, onMounted } from 'vue';
+import {useAnimation, usePlayer, useWidget} from '@vuemotion/vue-core'
+import {Car, Arc, move} from '@vuemotion/vue-lib'
+import {inject, nextTick, onMounted, provide, ref} from 'vue';
 
 const { widget, props } = useWidget()
 const player = usePlayer()
 player.play()
 
 onMounted(() => {
+  useAnimation(props, player).animate(move({duration: 1, offsetX: 100}))
   nextTick(() => {
     console.log(props)
+    props.x = 100
   })
 })
 
@@ -17,7 +19,7 @@ onMounted(() => {
 
 <template>
   <Car :width="1600" :height="800">
-    <Arc :radius="100" ref="widget"/>
+    <Arc :radius="100" :x="500" ref="widget"/>
   </Car>
 </template>
 
