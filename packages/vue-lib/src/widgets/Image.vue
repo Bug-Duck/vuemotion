3d3b3b8e707a9bf10816c932a2bf73dbe4a5a055
青椒肉丝 <58922004+pkc918@users.noreply.github.com>
2024-08-25 09:04:08 +0800

    feat: added Image component for svg (#1)
    * feat: added Image component for svg
    
    * chore: delete `<style>`

---
+++ packages/vue-lib/src/widgets/Image.vue
@@ -0,0 +1,18 @@
+<template>
+  <BaseWidget :scaleX="options.scaleX" :scaleY="options.scaleY" :rotation="options.rotation" :x="options.x" :y="options.y">
+    <image :href="options.href" :height="options.height" :width="options.width" :decoding="options.decoding" :preserve-aspect-ratio="options.preserveAspectRatio" :x="options.x" :y="options.y" :crossorigin="options.crossorigin" />
+  </BaseWidget>
+</template>
+
+<script lang="ts" setup>
+import { defineProps } from "vue";
+import { ImageOptions } from "./image.ts";
+import { defineWidget } from "@vuemotion/vue-core";
+import BaseWidget from "./base-widget.vue";
+
+const props = withDefaults(defineProps<ImageOptions>(), {
+  decoding: "auto",
+  preserveAspectRatio: "xMidYMid"
+});
+const options = defineWidget<ImageOptions>(props);
+</script>
