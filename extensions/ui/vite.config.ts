d3ee71a47acd1f476f28433c22aec44193404a52
montmorill <montmorillonite@qq.com>
2024-08-27 11:43:47 +0800

    move: package/extensions => /extensions

--- extensions/ui/vite.config.ts
+++ extensions/ui/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: 'src/index.ts',
-      name: 'NewcarVue',
+      name: 'VueMotion',
       formats: ['es', 'cjs'],
       fileName: 'index',
     },
