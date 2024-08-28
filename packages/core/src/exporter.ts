d5626c761312de033252e34843cd6385634026d7
Acbox liu <850625057@qq.com>
2024-08-28 09:41:20 +0800

    feat: improve pixels

--- packages/core/src/exporter.ts
+++ packages/core/src/exporter.ts
@@ -12,9 +12,10 @@ export function useExporter(selector: string, player: ReturnType<typeof usePlaye
       const svg = new XMLSerializer().serializeToString(motion as SVGElement);
       const blob = new Blob([svg], { type: 'image/svg+xml' })
       const canvas = document.createElement('canvas')
-      canvas.width = motion.clientWidth
-      canvas.height = motion.clientHeight
+      canvas.width = motion.clientWidth * 3
+      canvas.height = motion.clientHeight * 3
       const ctx = canvas.getContext('2d')
+      ctx?.scale(3, 3)
       const img = new Image()
       img.src = URL.createObjectURL(blob)
       await new Promise(res => img.onload = res)
