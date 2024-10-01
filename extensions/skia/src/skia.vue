5c372935eba00b11a7b04ff9bed43a06f9f97934
Acbox liu <850625057@qq.com>
2024-10-01 13:53:06 +0800

    feat:(10%): skia extension

---
+++ extensions/skia/src/skia.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { defineWidget } from '@vue-motion/core';
+import type { WidgetOptions } from '@vue-motion/lib'
+import { onBeforeMount, ref } from 'vue';
+import type { Surface } from 'canvaskit-wasm'
+import CanvasKitInit from 'canvaskit-wasm';
+
+export interface SkiaOptions extends WidgetOptions {
+  width: number
+  height: number
+  wasmFile: string
+}
+
+const props = defineProps<SkiaOptions>()
+const options = defineWidget<SkiaOptions>(props)
+
+const ck = await CanvasKitInit({
+  locateFile(_file) {
+    return options.wasmFile
+  },
+})
+const canvas = new OffscreenCanvas(options.width, options.height)
+const surface = ck.MakeWebGLCanvasSurface(canvas as unknown as HTMLCanvasElement)
+
+const url = ref('')
+
+const surfaceProxy = new Proxy(surface!, {
+  async apply(target: any, _thisArg, argArray) {
+    url.value = window.URL.createObjectURL(await canvas.convertToBlob())
+    return target(...argArray)
+  },
+})
+
+</script>
