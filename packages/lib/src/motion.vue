0874a1c2c66ec1ffd92078c1dc1e4faab3b23e53
Acbox liu <850625057@qq.com>
2024-10-05 21:58:17 +0800

    fix: incorrect coordinates

--- packages/lib/src/motion.vue
+++ packages/lib/src/motion.vue
@@ -17,10 +17,10 @@ provide('motion-height', props.height)
 <template>
   <svg
     :viewBox="`0 0 ${width} ${height}`" style="background-color: black;" :style="{
-      transform: `scale(${props.scale})`,
-      minWidth: `${props.minWidth}px`,
-      minHeight: `${props.minHeight}px`,
-    }"
+      transform: `scale(${props.scale ?? 1})`,
+      minWidth: `${props.minWidth ?? width}px`,
+      minHeight: `${props.minHeight ?? height}px`,
+    }" :width="width" :height="height"
   >
     <g v-bind="figure({ x: width / 2, y: height / 2 })">
       <slot />
