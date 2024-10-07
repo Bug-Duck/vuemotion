c01702858b4cfbe0ee6cb0c971a5885581934f61
Dylan Su <87633881+xs10l3@users.noreply.github.com>
2024-10-07 13:38:28 +0800

    Revert "fix(core):animation (#39)"
    This reverts commit 58a5506e7ed37c9463ef4c1b40f7ab3b23917d9d.

--- packages/core/src/animation.ts
+++ packages/core/src/animation.ts
@@ -1,189 +1,111 @@
-import type { WatchSource } from 'vue'
+import type { Ref } from 'vue'
 import { watch } from 'vue'
 
-/**
- * Defines a timing function type that takes a number and returns a number.
- */
 export type TimingFunction = (x: number) => number
-
-/**
- * Linear timing function that returns the input value.
- */
 export const linear: TimingFunction = x => x
 
-/**
- * Defines an animation type that performs an animation step on a target object.
- * @template T The type of the target object.
- * @template A The type of the context object.
- * @param target The target object to animate.
- * @param context Additional context information for the animation.
- * @param progress The progress of the animation, ranging from 0 to 1.
- */
-export type Animation<T extends object, A extends object = object> =
-  (target: T, context: A, progress: number) => void
+export interface Context<T extends object> {
+  target: T
+}
 
-/**
- * Defines an animation instance type with specific configurations and states.
- * @template T The type of the target object.
- * @template A The type of the context object.
- */
-type AnimationInstance<T extends object, A extends object> = {
-  context: A
-  animation: Animation<T, A>
+export type Animation<A extends object, T extends object = object> =
+  (context: Context<T> & A, progress: number) => void
+
+export function defineAnimation<
+  A extends object,
+  T extends object,
+>(animation: Animation<A, T>): Animation<A, T> {
+  return animation
+}
+
+export function exec<T extends object>(callback: (widget: T) => void) {
+  return defineAnimation((context, progress) => {
+    if (progress === 0)
+      callback(context.target as T)
+  })
+}
+
+export const delay = defineAnimation((_context, _progress) => {
+  // do nothing
+})
+
+interface AnimationInstance<A extends object, T extends object> {
+  context: Context<T> & A
+  animation: Animation<A, T>
   startAt?: number
-  duration: number | 'once'
+  duration: number
   by: TimingFunction
 }
 
-/**
- * AnimationManager class manages and executes an animation queue.
- * @template T The type of the target object.
- */
 export class AnimationManager<T extends object> {
-  animations: AnimationInstance<T, any>[] = []
+  animations: AnimationInstance<any, T>[] = []
+  index: number = 0
 
-  /**
-   * Constructor initializes the AnimationManager with a target and an elapsed time watcher.
-   * @param target The target object to be animated.
-   * @param elapsed The source of elapsed time to watch for animation updates.
-   */
-  constructor(target: T, elapsed: WatchSource<number>) {
+  constructor(public target: T, elapsed: Ref<number>) {
     watch(elapsed, <A extends object>(elapsed: number) => {
-      if (this.animations.length === 0) {
+      if (this.animations.length === 0)
         return
+      const animation: AnimationInstance<A, T> = this.animations[0]
+      if (typeof animation.startAt === 'undefined') {
+        animation.startAt = elapsed
+        animation.animation(animation.context, 0)
       }
-
-      const {
-        context,
-        animation,
-        duration,
-        startAt,
-        by
-      }: AnimationInstance<T, A> = this.animations[0]
-
-      if (startAt === undefined) {
-        this.animations[0].startAt = elapsed
-        animation(target, context, 0)
-        if (duration === 'once') {
-          this.animations.shift()
-        }
-      } else {
-        const progress = (elapsed - startAt) / (duration as number)
-        animation(target, context, by(Math.min(progress, 1)))
+      else {
+        const progress = (elapsed - animation.startAt) / (animation.duration ?? (elapsed - animation.startAt))
+        animation.animation(animation.context, animation.by(Math.min(progress, 1)))
         if (progress >= 1) {
           this.animations.shift()
+          // this.index++
         }
       }
     })
   }
 
-  /**
-   * Adds an animation to the queue.
-   * @param animation The animation function to execute.
-   * @param context Additional context for the animation.
-   * @returns The current AnimationManager instance for chaining.
-   */
-  animate<A extends object>(animation: Animation<T, A>, context: {
-    duration?: AnimationInstance<T, A>['duration']
+  animate<A extends object>(animation: Animation<A, T>, context?: {
+    duration?: number
     by?: TimingFunction
   } & A) {
     context ??= {} as A
-    const duration = context.duration ?? 1
     const by = context.by ?? linear
+    const duration = context.duration ?? 1
+    Object.assign(context, { target: this.target })
     this.animations.push({ context, animation, duration, by })
+
     return this
   }
 
-  /**
-   * Adds a one-time animation to the queue.
-   * @param animation The animation function to execute once.
-   * @returns The current AnimationManager instance for chaining.
-   */
-  once(animation: (target: T) => void) {
-    this.animate(animation, { duration: 'once' })
+  exec(callback: (widget: T) => void) {
+    this.animate(exec(callback), { duration: 0 })
+
     return this
   }
 
-  /**
-   * Adds a delay animation to the queue.
-   * @param duration The duration of the delay in milliseconds.
-   * @returns The current AnimationManager instance for chaining.
-   */
   delay(duration: number) {
-    this.animate(() => { /* empty animation */ }, { duration })
+    this.animate(delay, { duration })
+
     return this
   }
 }
 
-/**
- * Function to define an animation.
- * @template T The type of the target object.
- * @template A The type of the context object.
- * @param animation The animation function.
- * @returns The defined animation function.
- */
-export function defineAnimation<
+export function withDefaults<
+  A extends object,
   T extends object,
-  A extends object = object,
->(animation: Animation<T, A>): Animation<T, A>
-
-/**
- * Function to define an animation with setup and disposal functions.
- * @template T The type of the target object.
- * @template A The type of the context object.
- * @param setup The setup function to initialize the animation.
- * @param dispose The disposal function to clean up after the animation.
- * @returns The defined animation function.
- */
-export function defineAnimation<
-  T extends object,
-  A extends object = object,
->(
-  setup: (target: T, context: A) => (progress: number) => void,
-  dispose?: (target: T, context: A) => void
-): Animation<T, A>
-
-/**
- * Overload function to define an animation based on the provided arguments.
- * @template T The type of the target object.
- * @template A The type of the context object.
- * @param args The arguments to define the animation.
- * @returns The defined animation function.
- */
-export function defineAnimation<
-  T extends object,
-  A extends object = object,
->(...args: [Animation<T, A>] | Parameters<typeof defineAnimation>) {
-  if (args[0].length === 3) {
-    return args[0] as Animation<T, A>
-  }
-  const [setup, dispose] = args as Parameters<typeof defineAnimation>
-  let animation: (progress: number) => void
-  return function (target: T, context: A, progress: number) {
+>(animation: Animation<A, T>, defaults: A): Animation<A, T> {
+  return function (context: Context<T> & A, progress: number) {
     if (progress === 0) {
-      animation = setup(target, context)
+      Object.assign(context, defaults)
     }
-    animation(progress)
-    if (dispose && progress === 1) {
-      dispose(target, context)
-    }
+    animation(context, progress)
   }
 }
 
-/**
- * Function to create a parallel animation that runs multiple animations simultaneously.
- * @template T The type of the target object.
- * @template A The type of the context object.
- * @param animations An array of animations to run in parallel.
- * @returns The combined parallel animation function.
- */
 export function parallel<
+  A extends object,
   T extends object,
-  A extends object = object,
->(...animations: Animation<T, A>[]): Animation<T, A> {
-  return function (target: T, context: A, progress: number) {
+>(...animations: Animation<A, T>[]): Animation<A, T> {
+  return function (context: Context<T> & A, progress: number) {
     for (const animation of animations) {
-      animation(target, context, progress)
+      animation(context, progress)
     }
   }
 }
