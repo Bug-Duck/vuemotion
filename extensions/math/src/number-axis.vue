f9f8ece79a68ff9246d47a2db6a11fcb5f1ccd46
Dylan Su <87633881+xs10l3@users.noreply.github.com>
2024-10-12 22:35:43 +0800

    fix(math): rangs & domain mix

--- extensions/math/src/number-axis.vue
+++ extensions/math/src/number-axis.vue
@@ -11,67 +11,88 @@ export interface NumberAxisOptions extends WidgetOptions, Growable {
   trim?: string // Arrow trim color or 'none'
   fontColor?: string // Font color
   fontSize?: number // Font size
-  ranges: [number, number]
+  domain: [number, number]
 }
 
 const props = defineProps<NumberAxisOptions>()
 const options = defineWidget<NumberAxisOptions>(props)
+
+function getIntervalOrDefault(interval?: number): number {
+  return interval ?? 100;
+}
+
+function getProgressOrDefault(progress?: number): number {
+  return progress ?? 1;
+}
 </script>
 
 <template>
   <g v-if="options.baseUnit === 'radian'" v-bind="widget(options)">
     <Line
-      :from="[options.ranges[0] * (options.interval ?? 100) * (options.progress ?? 1), 0]"
-      :to="[options.ranges[1] * (options.interval ?? 100) * (options.progress ?? 1), 0]"
-      :border-color="trim ?? 'white'"
+      :from="[options.domain[0] * getIntervalOrDefault(options.interval) * getProgressOrDefault(options.progress), 0]"
+      :to="[options.domain[1] * getIntervalOrDefault(options.interval) * getProgressOrDefault(options.progress), 0]"
+      :border-color="options.trim ?? 'white'"
     />
     <Line
-      v-for="i in Math.floor(Math.abs(options.ranges[1] - options.ranges[0]) / Math.PI * 2)"
+      v-for="i in Math.floor(Math.abs(options.domain[1] - options.domain[0]) / Math.PI * 2)"
       :from="[(i + 1) * (options.interval ?? Math.PI * 50), -10]" :to="[(i + 1) * (options.interval ?? Math.PI * 50), 10]"
-      :border-color="trim ?? 'white'" :x="(options.ranges[0] - 1) * (options.interval ?? Math.PI * 50)"
+      :border-color="trim ?? 'white'" :x="(options.domain[0] - 1) * (options.interval ?? Math.PI * 50)"
     />
     <Text
-      v-for="i in Math.floor(Math.abs(options.ranges[1] - options.ranges[0]) / Math.PI * 2)" border-color="none"
-      :fill-color="options.fontColor ?? 'white'" :x="(options.ranges[0] + i) * (options.interval ?? Math.PI * 50)" :y="20"
+      v-for="i in Math.floor(Math.abs(options.domain[1] - options.domain[0]) / Math.PI * 2)" border-color="none"
+      :fill-color="options.fontColor ?? 'white'" :x="(options.domain[0] + i) * (options.interval ?? Math.PI * 50)" :y="20"
       :rotation="-(options.rotation ?? 0)" align="start" :font-size="20"
     >
-      {{ (options.trend ?? (x => (x).toString()))((i + options.ranges[0]) / 2) }} π
+      {{ (options.trend ?? (x => (x).toString()))((i + options.domain[0]) / 2) }} π
     </Text>
     <Polygon
       :points="[
         [0, -10],
         [0, 10],
         [Math.sqrt(300), 0],
-      ]" :border-color="options.tip ?? 'white'" :fill-color="options.tip ?? 'white'"
-      :x="options.ranges[1] * (options.interval ?? 100)" :y="0"
+      ]" 
+      :border-color="options.tip ?? 'white'" 
+      :fill-color="options.tip ?? 'white'"
+      :x="options.domain[1] * getIntervalOrDefault(options.interval)" 
+      :y="0"
     />
   </g>
   <!-- ToDo: Add support for degree -->
   <g v-else v-bind="widget(options)">
     <Line
-      :from="[options.ranges[0] * (options.interval ?? 100) * (options.progress ?? 1), 0]"
-      :to="[options.ranges[1] * (options.interval ?? 100) * (options.progress ?? 1), 0]"
-      :border-color="trim ?? 'white'"
+      :from="[options.domain[0] * getIntervalOrDefault(options.interval) * getProgressOrDefault(options.progress), 0]"
+      :to="[options.domain[1] * getIntervalOrDefault(options.interval) * getProgressOrDefault(options.progress), 0]"
+      :border-color="options.trim ?? 'white'"
     />
     <Line
-      v-for="i in Math.abs(options.ranges[1] - options.ranges[0])" :from="[(i + 1) * (options.interval ?? 100), -10]"
-      :to="[(i + 1) * (options.interval ?? 100), 10]" :border-color="trim ?? 'white'"
-      :x="(options.ranges[0] - 1) * (options.interval ?? 100)"
+      v-for="i in Math.abs(options.domain[1] - options.domain[0])" 
+      :from="[(i + 1) * getIntervalOrDefault(options.interval), -10]" 
+      :to="[(i + 1) * getIntervalOrDefault(options.interval), 10]" 
+      :border-color="options.trim ?? 'white'" 
+      :x="(options.domain[0] - 1) * getIntervalOrDefault(options.interval)"
     />
     <Text
-      v-for="i in Math.abs(options.ranges[1] - options.ranges[0] + 1)" border-color="none"
-      :fill-color="options.fontColor ?? 'white'" :x="(options.ranges[0] + i - 1) * (options.interval ?? 100)" :y="20"
-      :rotation="-(options.rotation ?? 0)" align="start" :font-size="20"
+      v-for="i in Math.abs(options.domain[1] - options.domain[0] + 1)" 
+      border-color="none"
+      :fill-color="options.fontColor ?? 'white'" 
+      :x="(options.domain[0] + i - 1) * getIntervalOrDefault(options.interval)" 
+      :y="20"
+      :rotation="-(options.rotation ?? 0)" 
+      align="start" 
+      :font-size="20"
     >
-      {{ (options.trend ?? (x => x.toString()))(i + options.ranges[0] - 1) }}
+      {{ (options.trend ?? (x => x.toString()))(i + options.domain[0] - 1) }}
     </Text>
     <Polygon
       :points="[
         [0, -10],
         [0, 10],
         [Math.sqrt(300), 0],
-      ]" :border-color="options.tip ?? 'white'" :fill-color="options.tip ?? 'white'"
-      :x="options.ranges[1] * (options.interval ?? 100)" :y="0"
+      ]" 
+      :border-color="options.tip ?? 'white'" 
+      :fill-color="options.tip ?? 'white'"
+      :x="options.domain[1] * getIntervalOrDefault(options.interval)" 
+      :y="0"
     />
   </g>
 </template>
--- extensions/math/src/number-plane.vue
+++ extensions/math/src/number-plane.vue
@@ -1,56 +1,105 @@
 <script setup lang="ts">
-import type { Growable, WidgetOptions } from '@vue-motion/lib'
-import { Line, Text, widget } from '@vue-motion/lib'
-import { defineWidget } from '@vue-motion/core'
-import { NumberAxis } from '.'
+import type { Growable, WidgetOptions } from "@vue-motion/lib";
+import { Line, Text, widget } from "@vue-motion/lib";
+import { defineWidget } from "@vue-motion/core";
+import { NumberAxis } from ".";
 
 export interface NumberPlaneOptions extends WidgetOptions, Growable {
-  intervalX?: number
-  trendX?: (count: number) => string
-  tipX?: string // Arrow tip color or 'none'
-  trimX?: string // Arrow trim color or 'none'
-  fontColorX?: string // Font color
-  fontSizeX?: number // Font size
-  rangesX: [number, number]
-  intervalY?: number
-  trendY?: (count: number) => string
-  tipY?: string // Arrow tip color or 'none'
-  trimY?: string // Arrow trim color or 'none'
-  fontColorY?: string // Font color
-  fontSizeY?: number // Font size
-  rangesY: [number, number]
-  baseUnit?: 'number' | 'radian' | 'degree'
-  grid?: boolean
-  gridColor?: string
-  gridWidth?: number
+  intervalX?: number;
+  trendX?: (count: number) => string;
+  tipX?: string; // Arrow tip color or 'none'
+  trimX?: string; // Arrow trim color or 'none'
+  fontColorX?: string; // Font color
+  fontSizeX?: number; // Font size
+  domainX: [number, number];
+  intervalY?: number;
+  trendY?: (count: number) => string;
+  tipY?: string; // Arrow tip color or 'none'
+  trimY?: string; // Arrow trim color or 'none'
+  fontColorY?: string; // Font color
+  fontSizeY?: number; // Font size
+  domainY: [number, number];
+  baseUnit?: "number" | "radian" | "degree";
+  grid?: boolean;
+  gridColor?: string;
+  gridWidth?: number;
+  Yrotation?: number;
 }
 
-const props = defineProps<NumberPlaneOptions>()
-const options = defineWidget<NumberPlaneOptions>(props)
+const props = defineProps<NumberPlaneOptions>();
+const options = defineWidget<NumberPlaneOptions>(props);
+
+const Yrotation = options.Yrotation ?? 90;
+const domain = [options.domainX.sort(), options.domainY.sort()];
 </script>
 
 <template>
   <g v-bind="widget(options)">
-    <NumberAxis :base-unit="options.baseUnit" :interval="options.intervalX"
-      :trend="(x => x === 0 ? '' : (options.trendY ?? (x => x.toString()))(x))" :tip="options.tipX ?? 'white'"
-      :trim="options.trimX ?? 'white'" :font-color="options.fontColorX ?? 'white'" :font-size="options.fontSizeX"
-      :ranges="options.rangesX" :progress="options.progress ?? 1" />
-    <NumberAxis :interval="options.intervalY ?? 100" :tip="options.tipY ?? 'white'" :trim="options.trimY ?? 'white'"
-      :font-color="options.fontColorY ?? 'white'" :font-size="options.fontSizeY" :ranges="options.rangesY"
-      :rotation="-90" :trend="(x => x === 0 ? '' : (options.trendY ?? (x => x.toString()))(x))"
-      :progress="options.progress ?? 1" />
-    <Text :fill-color="options.fontColorX ?? 'white'" :font-size="20" :x="20" :y="20">
+    <NumberAxis
+      :base-unit="options.baseUnit"
+      :interval="options.intervalX"
+      :trend="
+        (x) => (x === 0 ? '' : (options.trendX ?? ((x) => x.toString()))(x))
+      "
+      :tip="options.tipX ?? 'white'"
+      :trim="options.trimX ?? 'white'"
+      :font-color="options.fontColorX ?? 'white'"
+      :font-size="options.fontSizeX"
+      :domain="domain[0]"
+      :progress="options.progress ?? 1"
+    />
+    <NumberAxis
+      :interval="options.intervalY ?? 100"
+      :tip="options.tipY ?? 'white'"
+      :trim="options.trimY ?? 'white'"
+      :font-color="options.fontColorY ?? 'white'"
+      :font-size="options.fontSizeY"
+      :domain="domain[1]"
+      :rotation="-Yrotation"
+      :trend="
+        (x) => (x === 0 ? '' : (options.trendY ?? ((x) => x.toString()))(x))
+      "
+      :progress="options.progress ?? 1"
+    />
+    <Text
+      :fill-color="options.fontColorX ?? 'white'"
+      :font-size="20"
+      :x="20"
+      :y="20"
+    >
       0
     </Text>
-    <Line v-if="options.grid" v-for="i in Math.abs(options.rangesX[1] - options.rangesX[0]) + 1"
-      :from="[i * (options.intervalX ?? 100), options.rangesY[0] * (options.intervalY ?? 100)]"
-      :to="[i * (options.intervalX ?? 100), options.rangesY[1] * (options.intervalY ?? 100)]"
-      :border-color="options.gridColor ?? 'white'" :border-width="options.gridWidth ?? 1"
-      :x="(options.rangesX[0] - 1) * (options.intervalX ?? 100)" :progress="options.progress ?? 1" />
-    <Line v-if="options.grid" v-for="i in Math.abs(options.rangesY[1] - options.rangesY[0]) + 1"
-      :from="[options.rangesX[0] * (options.intervalX ?? 100), i * (options.intervalY ?? 100)]"
-      :to="[options.rangesX[1] * (options.intervalX ?? 100), i * (options.intervalY ?? 100)]"
-      :border-color="options.gridColor ?? 'white'" :border-width="options.gridWidth ?? 1"
-      :y="(options.rangesY[0] - 1) * (options.intervalY ?? 100)" :progress="options.progress ?? 1" />
+    <Line
+      v-if="options.grid"
+      v-for="i in domain[0][1] - domain[0][0]"
+      :from="[
+        i * (options.intervalX ?? 100),
+        options.domainY[0] * (options.intervalY ?? 100),
+      ]"
+      :to="[
+        i * (options.intervalX ?? 100),
+        options.domainY[1] * (options.intervalY ?? 100),
+      ]"
+      :border-color="options.gridColor ?? 'white'"
+      :border-width="options.gridWidth ?? 1"
+      :x="(options.domainX[0] - 1) * (options.intervalX ?? 100)"
+      :progress="options.progress ?? 1"
+    />
+    <Line
+      v-if="options.grid"
+      v-for="i in domain[1][1] - domain[1][0]"
+      :from="[
+        options.domainX[0] * (options.intervalX ?? 100),
+        i * (options.intervalY ?? 100),
+      ]"
+      :to="[
+        options.domainX[1] * (options.intervalX ?? 100),
+        i * (options.intervalY ?? 100),
+      ]"
+      :border-color="options.gridColor ?? 'white'"
+      :border-width="options.gridWidth ?? 1"
+      :y="(options.domainY[0] - 1) * (options.intervalY ?? 100)"
+      :progress="options.progress ?? 1"
+    />
   </g>
 </template>
