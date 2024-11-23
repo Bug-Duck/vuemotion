8260662d47e1054feff06e60ceb569981985cb79
sujiaqi <1526394815@qq.com>
2024-11-23 23:15:30 +0800

    feat(math): Add Ellipse Component -Implemented a new Ellipse Component for drawing ellipse shapes in the Math Extension -The component supports custom properties such as center point, long axis, and short axis -Use the definitions and properties of the Vue Motion library to implement component functionality

---
+++ packages/lib/src/widgets/ellipse.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+import { defineWidget } from '@vue-motion/core'
+import type { FigureMixin, FigureOptions } from './figure'
+import { figure } from './figure'
+
+export interface EllipseOptions extends FigureOptions {
+  cx: number
+  cy: number
+  rx: number
+  ry: number
+  width?: number
+  height?: number
+  radius?: number
+}
+export type EllipseMixin = EllipseOptions & FigureMixin
+
+const props = defineProps<EllipseOptions>()
+const options = defineWidget(props)
+</script>
+
+<template>
+  <ellipse
+    v-bind="figure(options)"
+    :cx="options.cx"
+    :cy="options.cy"
+    :rx="options.rx"
+    :ry="options.ry"
+    :width="options.width"
+    :height="options.height"
+  />
+</template>
