a4dac3ede717ce98bcc531a0e7c94ed1b38ac41b
sujiaqi <1526394815@qq.com>
2024-11-26 23:31:30 +0800

    feat(widgets): add Circle component- Create Circle component for rendering circle SVG elements - Define CircleOptions interface with r, cx, and cy properties- Implement CircleMixin for circle-specific functionality - Update widgets index to export Circle component and related types

---
+++ packages/lib/src/widgets/circle.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+import { defineWidget } from '@vue-motion/core'
+import type { FigureMixin, FigureOptions } from './figure'
+import { figure } from './figure'
+
+export interface CircleOptions extends FigureOptions {
+  r: number
+  cx: number
+  cy: number
+}
+export type CircleMixin = CircleOptions & FigureMixin
+
+const props = defineProps<CircleOptions>()
+const options = defineWidget(props)
+</script>
+
+<template>
+  <circle
+    :cx="options.cx"
+    :cy="options.cy"
+    :r="options.r"
+    v-bind="figure(options)"
+  />
+</template>
