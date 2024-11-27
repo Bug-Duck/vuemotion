9c199d17d94ebee7b06773d1db0b9a1c2a14ee8f
Thomas Wang <55052188+cxzlw@users.noreply.github.com>
2024-11-27 22:40:00 +0800

    build/ci: setup linter and formatter to workflow (#43)
    * build: setup linter and formatter
    
    Squashed commits:
    
    build: reconfigure lintstagedrc to avoid running prettier on unexpected files
    
    build: add prettier, update and config husky
    
    * ci: add eslint and prettier check

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,17 @@
-import antfu from '@antfu/eslint-config'
+import pluginJs from "@eslint/js";
+import pluginVue from "eslint-plugin-vue";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
-export default antfu({
-  typescript: true,
-  vue: true,
-
-  overrides: {
-    stylistic: {
-      'ts/array-type': 'warn',
-      'style/arrow-parens': 'error',
-      // 'ts/explicit-function-return-type': 'error',
-    },
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts,vue}"] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...pluginVue.configs["flat/essential"],
+  {
+    files: ["**/*.vue"],
+    languageOptions: { parserOptions: { parser: tseslint.parser } },
   },
-})
+];
