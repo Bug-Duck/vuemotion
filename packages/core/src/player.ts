f9ecdf347e2e32cc8406df64fd13d6c695567b38
cxzlw <wsc_zds@qq.com>
2024-11-29 00:00:19 +0800

    fix: add wid and ReturnWidget

--- packages/core/src/player.ts
+++ packages/core/src/player.ts
@@ -1,64 +1,64 @@
-import type { App, Ref } from 'vue'
-import { inject, ref } from 'vue'
-import { AnimationManager } from './animation'
+import type { App, Ref } from "vue";
+import { inject, ref } from "vue";
+import { AnimationManager } from "./animation";
 
 export function createPlayer(options: {
-  studio?: boolean
-  fps?: number
-  addition?: (() => void)[]
+  studio?: boolean;
+  fps?: number;
+  addition?: (() => void)[];
 }) {
   return {
     install(app: App) {
-      app.provide('elapsed', ref(0))
-      app.provide('studio', options.studio ?? false)
-      app.provide('fps', options.fps ?? 60)
-      app.provide('ADDITION_ANIMATIONS', options.addition ?? [])
+      app.provide("elapsed", ref(0));
+      app.provide("studio", options.studio ?? false);
+      app.provide("fps", options.fps ?? 60);
+      app.provide("ADDITION_ANIMATIONS", options.addition ?? []);
     },
-  }
+  };
 }
 
 export function usePlayer() {
-  const startAt = performance.now() / 1000
-  const elapsed = inject('elapsed') as Ref<number>
-  const studio = inject('studio') as boolean
-  const fps = inject('fps') as number
-  const playing = ref(false)
+  const startAt = performance.now() / 1000;
+  const elapsed = inject("elapsed") as Ref<number>;
+  const studio = inject("studio") as boolean;
+  const fps = inject("fps") as number;
+  const playing = ref(false);
 
   if (studio) {
-    document.addEventListener('click', () => {
-      elapsed.value += 1 / fps
-    })
+    document.addEventListener("click", () => {
+      elapsed.value += 1 / fps;
+    });
   }
 
   function play() {
     if (studio)
-    // eslint-disable-next-line no-console
-      return console.log('You are in the studio mode, you are not supposed to call this function!')
-    playing.value = true
-    elapsed.value = performance.now() / 1000 - startAt
-    if (playing.value)
-      requestAnimationFrame(play)
+      return console.log(
+        "You are in the studio mode, you are not supposed to call this function!",
+      );
+    playing.value = true;
+    elapsed.value = performance.now() / 1000 - startAt;
+    if (playing.value) requestAnimationFrame(play);
   }
 
   function useAnimation<T extends object>(widget: T) {
-    return new AnimationManager(widget, elapsed)
+    return new AnimationManager(widget, elapsed);
   }
 
   function setElapsed(value: number) {
-    elapsed.value = value
+    elapsed.value = value;
   }
 
   function pause() {
-    playing.value = false
+    playing.value = false;
   }
 
   function renderOnce(ela: number) {
     setTimeout(() => {
-      elapsed.value = ela
-    }, 1000)
+      elapsed.value = ela;
+    }, 1000);
   }
 
-  return { play, elapsed, useAnimation, setElapsed, pause, renderOnce }
+  return { play, elapsed, useAnimation, setElapsed, pause, renderOnce };
 }
 
-export type Player = ReturnType<typeof usePlayer>
+export type Player = ReturnType<typeof usePlayer>;
--- packages/core/src/widget.ts
+++ packages/core/src/widget.ts
@@ -1,43 +1,50 @@
-import type { InjectionKey, Reactive, Ref, Slots } from 'vue'
-import { getCurrentInstance, onMounted, provide, reactive, ref, useSlots } from 'vue'
-import type { AnimationManager } from './animation'
+import type { InjectionKey, Reactive, Ref, Slots } from "vue";
+import {
+  getCurrentInstance,
+  onMounted,
+  provide,
+  reactive,
+  ref,
+  useSlots,
+} from "vue";
+import type { AnimationManager } from "./animation";
 
-const childWidgets: InjectionKey<Ref<Widget[]>> = Symbol('child-widgets')
+const childWidgets: InjectionKey<Ref<Widget[]>> = Symbol("child-widgets");
 
 export interface Widget<T = any> {
-  widget?: Reactive<any>
-  element?: SVGElement
-  range?: DOMRect
-  slots?: Slots
-  children?: Widget<T>[]
-  manager?: AnimationManager<T>
+  widget?: Reactive<any>;
+  element?: SVGElement;
+  range?: DOMRect;
+  slots?: Slots;
+  children?: Widget<T>[];
+  manager?: AnimationManager<T>;
+  wid?: string;
 }
 
 export function useWidget<T extends Widget>(): Reactive<T> {
-  const widget = reactive({})
-  return widget as Reactive<T>
+  const widget = reactive({});
+  return widget as Reactive<T>;
 }
 
 export function useChildren(): Ref<Widget[]> {
-  const children = ref([])
-  provide(childWidgets, children)
-  return children
+  const children = ref([]);
+  provide(childWidgets, children);
+  return children;
 }
 
-export function defineWidget<T extends Widget>(
-  props: T,
-  root?: SVGElement,
-) {
-  const { widget } = props
+export function defineWidget<T extends Widget>(props: T, root?: SVGElement) {
+  const { widget } = props;
   if (widget) {
-    Object.assign(widget, props)
+    Object.assign(widget, props);
     onMounted(() => {
-      widget.element = root ?? getCurrentInstance()?.proxy?.$el.parentElement
-      widget.range = widget.element?.getBoundingClientRect()
-      widget.slots = useSlots()
+      widget.element = root ?? getCurrentInstance()?.proxy?.$el.parentElement;
+      widget.range = widget.element?.getBoundingClientRect();
+      widget.slots = useSlots();
       // inject(childWidgets)?.value?.push(widget as T)
-    })
-    return widget as T
+    });
+    return widget as T;
   }
-  return props as T
+  return props as T;
 }
+
+export type ReturnWidget<T extends Widget> = ReturnType<typeof defineWidget<T>>;
