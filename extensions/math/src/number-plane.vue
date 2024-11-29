eca2d4635af0cee7674a75847d5507f48d4026e7
cxzlw <wsc_zds@qq.com>
2024-11-29 21:53:59 +0800

    fix: v-for and v-if related errors

--- extensions/math/src/number-plane.vue
+++ extensions/math/src/number-plane.vue
@@ -1,36 +1,36 @@
 <script setup lang="ts">
-import type { Growable, WidgetOptions } from '@vue-motion/lib'
-import { Line, Text, widget } from '@vue-motion/lib'
-import { defineWidget } from '@vue-motion/core'
-import { NumberAxis } from '.'
+import { defineWidget } from "@vue-motion/core";
+import type { Growable, WidgetOptions } from "@vue-motion/lib";
+import { Line, Text, widget } from "@vue-motion/lib";
+import { NumberAxis } from ".";
 
 export interface NumberPlaneOptions extends WidgetOptions, Growable {
-  intervalX?: number
-  trendX?: (count: number) => string
-  tipX?: string // Arrow tip color or 'none'
-  trimX?: string // Arrow trim color or 'none'
-  fontColorX?: string // Font color
-  fontSizeX?: number // Font size
-  domainX: [number, number]
-  intervalY?: number
-  trendY?: (count: number) => string
-  tipY?: string // Arrow tip color or 'none'
-  trimY?: string // Arrow trim color or 'none'
-  fontColorY?: string // Font color
-  fontSizeY?: number // Font size
-  domainY: [number, number]
-  baseUnit?: 'number' | 'radian' | 'degree'
-  grid?: boolean
-  gridColor?: string
-  gridWidth?: number
-  yRotation?: number
+  intervalX?: number;
+  trendX?: (count: number) => string;
+  tipX?: string; // Arrow tip color or 'none'
+  trimX?: string; // Arrow trim color or 'none'
+  fontColorX?: string; // Font color
+  fontSizeX?: number; // Font size
+  domainX: [number, number];
+  intervalY?: number;
+  trendY?: (count: number) => string;
+  tipY?: string; // Arrow tip color or 'none'
+  trimY?: string; // Arrow trim color or 'none'
+  fontColorY?: string; // Font color
+  fontSizeY?: number; // Font size
+  domainY: [number, number];
+  baseUnit?: "number" | "radian" | "degree";
+  grid?: boolean;
+  gridColor?: string;
+  gridWidth?: number;
+  yRotation?: number;
 }
 
-const props = defineProps<NumberPlaneOptions>()
-const options = defineWidget<NumberPlaneOptions>(props)
+const props = defineProps<NumberPlaneOptions>();
+const options = defineWidget<NumberPlaneOptions>(props);
 
-const Yrotation = options.yRotation ?? 90
-const domain = [options.domainX.sort(), options.domainY.sort()]
+const Yrotation = options.yRotation ?? 90;
+const domain = [options.domainX.sort(), options.domainY.sort()];
 </script>
 
 <template>
@@ -69,37 +69,39 @@ const domain = [options.domainX.sort(), options.domainY.sort()]
     >
       0
     </Text>
-    <Line
-      v-for="i in domain[0][1] - domain[0][0]"
-      v-if="options.grid"
-      :from="[
-        i * (options.intervalX ?? 100),
-        options.domainY[0] * (options.intervalY ?? 100),
-      ]"
-      :to="[
-        i * (options.intervalX ?? 100),
-        options.domainY[1] * (options.intervalY ?? 100),
-      ]"
-      :border-color="options.gridColor ?? 'white'"
-      :border-width="options.gridWidth ?? 1"
-      :x="(options.domainX[0] - 1) * (options.intervalX ?? 100)"
-      :progress="options.progress ?? 1"
-    />
-    <Line
-      v-for="i in domain[1][1] - domain[1][0]"
-      v-if="options.grid"
-      :from="[
-        options.domainX[0] * (options.intervalX ?? 100),
-        i * (options.intervalY ?? 100),
-      ]"
-      :to="[
-        options.domainX[1] * (options.intervalX ?? 100),
-        i * (options.intervalY ?? 100),
-      ]"
-      :border-color="options.gridColor ?? 'white'"
-      :border-width="options.gridWidth ?? 1"
-      :y="(options.domainY[0] - 1) * (options.intervalY ?? 100)"
-      :progress="options.progress ?? 1"
-    />
+    <g v-if="options.grid">
+      <Line
+        v-for="i in domain[0][1] - domain[0][0]"
+        :key="i"
+        :from="[
+          i * (options.intervalX ?? 100),
+          options.domainY[0] * (options.intervalY ?? 100),
+        ]"
+        :to="[
+          i * (options.intervalX ?? 100),
+          options.domainY[1] * (options.intervalY ?? 100),
+        ]"
+        :border-color="options.gridColor ?? 'white'"
+        :border-width="options.gridWidth ?? 1"
+        :x="(options.domainX[0] - 1) * (options.intervalX ?? 100)"
+        :progress="options.progress ?? 1"
+      />
+      <Line
+        v-for="i in domain[1][1] - domain[1][0]"
+        :key="i"
+        :from="[
+          options.domainX[0] * (options.intervalX ?? 100),
+          i * (options.intervalY ?? 100),
+        ]"
+        :to="[
+          options.domainX[1] * (options.intervalX ?? 100),
+          i * (options.intervalY ?? 100),
+        ]"
+        :border-color="options.gridColor ?? 'white'"
+        :border-width="options.gridWidth ?? 1"
+        :y="(options.domainY[0] - 1) * (options.intervalY ?? 100)"
+        :progress="options.progress ?? 1"
+      />
+    </g>
   </g>
 </template>
