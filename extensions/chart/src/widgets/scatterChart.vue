10d1aa17b0ae1efde28b9c9fbc40f4c727d07d6d
cxzlw <wsc_zds@qq.com>
2024-11-29 21:48:18 +0800

    fix: @typescript-eslint/no-empty-object-type

--- extensions/chart/src/widgets/scatterChart.vue
+++ extensions/chart/src/widgets/scatterChart.vue
@@ -1,24 +1,21 @@
 <script setup lang="ts">
-import { useSimpleChart } from '../utils/useSimpleChart.ts'
-import type { LineChartOptions } from '..'
-import { LineChart } from '..'
+import type { LineChartOptions } from "..";
+import { LineChart } from "..";
+import { useSimpleChart } from "../utils/useSimpleChart.ts";
 
-export interface ScatterChartOptions extends LineChartOptions {
-}
+export type ScatterChartOptions = LineChartOptions;
 
 const props = withDefaults(defineProps<ScatterChartOptions>(), {
   gridAlign: false,
-})
-const {
-  options,
-} = useSimpleChart<ScatterChartOptions>(props)
+});
+const { options } = useSimpleChart<ScatterChartOptions>(props);
 
-options.style ??= {}
+options.style ??= {};
 options.style = {
   showLine: false,
   animateIndex: true,
   ...options.style,
-}
+};
 </script>
 
 <template>
@@ -27,6 +24,4 @@ options.style = {
   </LineChart>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
