4b8a19b33cf881b354dc62f0370408f904ba32c3
cxzlw <wsc_zds@qq.com>
2024-11-29 22:00:51 +0800

    fix: add eslint disable for typst

--- extensions/typst/src/index.vue
+++ extensions/typst/src/index.vue
@@ -1,30 +1,36 @@
 <script setup lang="ts">
-import { defineWidget } from '@vue-motion/core'
-import { type WidgetOptions, widget } from '@vue-motion/lib'
-import { onMounted, ref, useSlots } from 'vue'
-import { $typst } from '@myriaddreamin/typst.ts/dist/esm/contrib/snippet'
+import { $typst } from "@myriaddreamin/typst.ts/dist/esm/contrib/snippet";
+import { defineWidget } from "@vue-motion/core";
+import { type WidgetOptions, widget } from "@vue-motion/lib";
+import { onMounted, ref, useSlots } from "vue";
 
 export interface TypstOptions extends WidgetOptions {
-  width?: number
-  height?: number
+  width?: number;
+  height?: number;
 }
 
-const props = defineProps<TypstOptions>()
-const options = defineWidget<TypstOptions>(props)
+const props = defineProps<TypstOptions>();
+const options = defineWidget<TypstOptions>(props);
 
-const compiled = ref()
+const compiled = ref();
 
-const slots = useSlots()
-const content = slots.default ? slots.default().map((vnode) => vnode.children).join('') : ''
+const slots = useSlots();
+const content = slots.default
+  ? slots
+      .default()
+      .map((vnode) => vnode.children)
+      .join("")
+  : "";
 
 onMounted(async () => {
-  console.log(content)
+  console.log(content);
   compiled.value = await $typst.svg({
     mainContent: content,
-  })
-})
+  });
+});
 </script>
 
 <template>
+  <!-- eslint-disable-next-line vue/no-v-text-v-html-on-component -->
   <g v-bind="widget(options)" v-html="compiled" />
 </template>
