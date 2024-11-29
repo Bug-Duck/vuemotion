53ae2661129bb6d09b3779c09980e67d3e206f81
cxzlw <wsc_zds@qq.com>
2024-11-29 21:36:50 +0800

    fix: @typescript-eslint/no-empty-object-type

--- extensions/chart/src/widgets/bubbleChart.vue
+++ extensions/chart/src/widgets/bubbleChart.vue
@@ -1,24 +1,21 @@
 <script setup lang="ts">
-import { useSimpleChart } from '../utils/useSimpleChart.ts'
-import type { LineChartOptions } from '..'
-import { LineChart } from '..'
+import type { LineChartOptions } from "..";
+import { LineChart } from "..";
+import { useSimpleChart } from "../utils/useSimpleChart.ts";
 
-export interface BubbleChartOptions extends LineChartOptions {
-}
+export type BubbleChartOptions = LineChartOptions;
 
 const props = withDefaults(defineProps<BubbleChartOptions>(), {
   gridAlign: false,
-})
-const {
-  options,
-} = useSimpleChart<BubbleChartOptions>(props)
+});
+const { options } = useSimpleChart<BubbleChartOptions>(props);
 
-options.style ??= {}
+options.style ??= {};
 options.style = {
   showLine: false,
   animateIndex: true,
   ...options.style,
-}
+};
 </script>
 
 <template>
@@ -27,6 +24,4 @@ options.style = {
   </LineChart>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
