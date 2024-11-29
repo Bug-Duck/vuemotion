501afcce042713ce80a68f2ffd67446f85bed07a
Acbox liu <850625057@qq.com>
2024-11-29 20:06:02 +0800

    feat: add animation grow

--- packages/lib/src/widgets/widget.ts
+++ packages/lib/src/widgets/widget.ts
@@ -1,16 +1,16 @@
 import type { AnimationManager, AnimationParams, Widget } from '@vue-motion/core'
 import { registerAnimation } from '@vue-motion/core'
 import { inject } from 'vue'
-import { type HasOpacity, type HasOpacityMixin, type HasScale, type HasScaleMixin, type Positional, type PositionalMixin, type Rotatable, type RotatableMixin, type Scalable, type StrokableMixin, moveOnFunction, moveOnPath } from '../animations'
+import { Growable, GrowableMixin, type HasOpacity, type HasOpacityMixin, type HasScale, type HasScaleMixin, type Positional, type PositionalMixin, type Rotatable, type RotatableMixin, type Scalable, type StrokableMixin, grow, moveOnFunction, moveOnPath } from '../animations'
 import { fadeIn, fadeOut, fadeTo, move, moveTo, rotate, rotateTo, scale, scaleTo, zoomIn, zoomOut, zoomTo } from '../animations'
 import type { Colorable, colorableMixin } from '../animations/color'
 import { discolorate } from '../animations/color'
 import type { Animation, AnimationInstance, TimingFunction } from '@vue-motion/core'
 
-export type WidgetOptions = Widget & Positional & Scalable & Rotatable & HasOpacity & Colorable & {
+export type WidgetOptions = Widget & Positional & Scalable & Rotatable & HasOpacity & Colorable & Growable & {
   manager?: AnimationManager<any>
 }
-export type WidgetMixin = WidgetOptions & PositionalMixin & RotatableMixin & HasScaleMixin & StrokableMixin & HasOpacityMixin & colorableMixin & {
+export type WidgetMixin = WidgetOptions & PositionalMixin & RotatableMixin & HasScaleMixin & StrokableMixin & HasOpacityMixin & colorableMixin & GrowableMixin & {
   parallel: (...animations: ((widget: WidgetMixin & { manager: AnimationManager<any> }) => void)[]) => void
   animate: <A>(animation: Animation<Widget, A>, context: {
     duration?: AnimationInstance<Widget, A>['duration']
@@ -49,11 +49,18 @@ export function widget(options: WidgetOptions) {
 
   const defaultDuration = 1
 
+  registerAnimation<Growable>('grow', (offsetX: number, offsetY: number, params?: AnimationParams) => {
+    return (manager) => manager.animate(grow, {
+      duration: params?.duration ?? defaultDuration,
+      by: params?.by ?? ((x) => x),
+    })
+  })
   registerAnimation<Positional>('move', (offsetX: number, offsetY: number, params?: AnimationParams) => {
     return (manager) => manager.animate(move, {
       offsetX,
       offsetY,
       duration: params?.duration ?? defaultDuration,
+      by: params?.by ?? ((x) => x),
     })
   })
   registerAnimation<Positional>('moveTo', (toX: number, toY: number, params?: AnimationParams) => {
@@ -61,6 +68,7 @@ export function widget(options: WidgetOptions) {
       toX,
       toY,
       duration: params?.duration ?? defaultDuration,
+      by: params?.by ?? ((x) => x),
     })
   })
   registerAnimation<Scalable>('scale', (offsetX: number, offsetY: number, params?: AnimationParams) => {
@@ -68,6 +76,7 @@ export function widget(options: WidgetOptions) {
       offsetX,
       offsetY,
       duration: params?.duration ?? defaultDuration,
+      by: params?.by ?? ((x) => x),
     })
   })
   registerAnimation<Scalable>('scaleTo', (toX: number, toY: number, params?: AnimationParams) => {
@@ -75,34 +84,40 @@ export function widget(options: WidgetOptions) {
       toX,
       toY,
       duration: params?.duration ?? defaultDuration,
+      by: params?.by ?? ((x) => x),
     })
   })
   registerAnimation<Rotatable>('rotate', (offset: number, params?: AnimationParams) => {
     return (manager) => manager.animate(rotate, {
       offset,
       duration: params?.duration ?? defaultDuration,
+      by: params?.by ?? ((x) => x),
     })
   })
   registerAnimation<Rotatable>('rotateTo', (to: number, params?: AnimationParams) => {
     return (manager) => manager.animate(rotateTo, {
       to,
       duration: params?.duration ?? defaultDuration,
+      by: params?.by ?? ((x) => x),
     })
   })
   registerAnimation<HasOpacity>('fadeTo', (to: number, params?: AnimationParams) => {
     return (manager) => manager.animate(fadeTo, {
       to,
       duration: params?.duration ?? defaultDuration,
+      by: params?.by ?? ((x) => x),
     })
   })
   registerAnimation<HasOpacity>('fadeIn', (params?: AnimationParams) => {
     return (manager) => manager.animate(fadeIn, {
       duration: params?.duration ?? defaultDuration,
+      by: params?.by ?? ((x) => x),
     })
   })
   registerAnimation<HasOpacity>('fadeOut', (params?: AnimationParams) => {
     return (manager) => manager.animate(fadeOut, {
       duration: params?.duration ?? defaultDuration,
+      by: params?.by ?? ((x) => x),
     })
   })
   registerAnimation<HasScale>('zoomTo', (toX: number, toY: number, params?: AnimationParams) => {
@@ -110,16 +125,19 @@ export function widget(options: WidgetOptions) {
       toX,
       toY,
       duration: params?.duration ?? defaultDuration,
+      by: params?.by ?? ((x) => x),
     })
   })
   registerAnimation<HasScale>('zoomIn', (params?: AnimationParams) => {
     return (manager) => manager.animate(zoomIn, {
       duration: params?.duration ?? defaultDuration,
+      by: params?.by ?? ((x) => x),
     })
   })
   registerAnimation<HasScale>('zoomOut', (params?: AnimationParams) => {
     return (manager) => manager.animate(zoomOut, {
       duration: params?.duration ?? defaultDuration,
+      by: params?.by ?? ((x) => x),
     })
   })
   registerAnimation<Colorable>('discolorate', (on: 'fill' | 'border' | 'color', offset: string, params?: AnimationParams) => {
@@ -127,18 +145,21 @@ export function widget(options: WidgetOptions) {
       offset,
       on,
       duration: params?.duration ?? defaultDuration,
+      by: params?.by ?? ((x) => x),
     })
   })
   registerAnimation<Positional>('moveOnPath', (path: string, params?: AnimationParams) => {
     return (manager) => manager.animate(moveOnPath, {
       path,
       duration: params?.duration ?? defaultDuration,
+      by: params?.by ?? ((x) => x),
     })
   })
   registerAnimation<Positional>('moveOnFunction', (path: (progress: number) => { x: number, y: number }, params?: AnimationParams) => {
     return (manager) => manager.animate(moveOnFunction, {
       path,
       duration: params?.duration ?? defaultDuration,
+      by: params?.by ?? ((x) => x),
     })
   })
   registerAnimation<Widget>('animate', <A>(animation: Animation<Widget, A>, context: {
@@ -153,7 +174,7 @@ export function widget(options: WidgetOptions) {
   registerAnimation<Widget>('delay', (duration: number) => {
     return (manager) => manager.delay(duration)
   })
-  registerAnimation<WidgetMixin>('parallel', 
+  registerAnimation<WidgetMixin>('parallel',
     <A>(...animations: ((widget: WidgetMixin & { manager: AnimationManager<any> }) => void)[]) => {
       return (manager: AnimationManager<WidgetMixin>) => {
         return manager.parallel(...animations)
